559be181d017d03f028faca488a9ef4d224db96a
Michiel TJampens <michiel.tjampens@vliz.be>
2021-04-28 13:59:49 +0200

    initial attempt for #16

--- src/util/database/SQLDB.java
+++ src/util/database/SQLDB.java
@@ -27,7 +27,7 @@ public class SQLDB extends Database{
     protected Connection con = null;
 
     DBTYPE type;
-    enum DBTYPE {MSSQL,MYSQL,MARIADB}
+    enum DBTYPE {MSSQL,MYSQL,MARIADB,POSTGRES}
 
     boolean busySimple =false;
     boolean busyPrepared =false;
@@ -62,6 +62,11 @@ public class SQLDB extends Database{
                 tableRequest="SHOW FULL TABLES;";
                 columnRequest="SHOW COLUMNS FROM ";
                 break;
+            case POSTGRES:
+                irl="jdbc:jdbc:postgresql://"+ address + (address.contains(":")?"/":":5432/")+dbName;
+                tableRequest="SELECT table_name FROM information_schema.tables WHERE NOT table_schema='pg_catalog'AND NOT table_schema='information_schema';";
+                columnRequest="SELECT * FROM information_schema.columns WHERE table_name=";
+                break;
         }
 
     }
@@ -75,6 +80,9 @@ public class SQLDB extends Database{
     }
     public static SQLDB asMYSQL( String address, String dbName, String user, String pass ){
         return new SQLDB( DBTYPE.MYSQL,address,dbName,user,pass);
+    }
+    public static SQLDB asPOSTGRES( String address, String dbName, String user, String pass ){
+        return new SQLDB( DBTYPE.POSTGRES,address,dbName,user,pass);
     }
     /* **************************************************************************************************/
     public String toString(){
@@ -119,6 +127,9 @@ public class SQLDB extends Database{
                     break;
                 case MARIADB: 
                     Class.forName("org.mariadb.jdbc.Driver");                       
+                    break;
+                case POSTGRES:
+                    Class.forName("org.postgresql.Driver");
                     break;
             }
 		} catch (ClassNotFoundException ex) {
@@ -491,6 +502,7 @@ public class SQLDB extends Database{
             case "mssql": db = SQLDB.asMSSQL(address, dbname, user, pass); break;
             case "mysql": db = SQLDB.asMYSQL(address, dbname, user, pass); break;
             case "mariadb": db = SQLDB.asMARIADB(address, dbname, user, pass); break;
+            case "postgres": db = SQLDB.asPOSTGRES(address, dbname, user, pass); break;
             default:
                 Logger.error("Invalid database type: "+type);
                 return null;         
