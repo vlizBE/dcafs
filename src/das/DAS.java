5f8e3f214623c936cf9671ef3efb393fb0707ac9
Michiel TJampens <michiel.tjampens@vliz.be>
2021-05-01 23:38:24 +0200

    version bump

--- src/das/DAS.java
+++ src/das/DAS.java
@@ -40,7 +40,7 @@ import java.util.concurrent.*;
 
 public class DAS implements DeadThreadListener {
 
-    private static final String version = "0.9.2";
+    private static final String version = "0.9.3";
 
     // Last date that changes were made
     Path settingsFile = Path.of("settings.xml");
