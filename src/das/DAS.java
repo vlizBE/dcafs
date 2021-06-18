b7dc248c0278b8b63e6515e963962037a71579ee
Michiel TJampens <michiel.tjampens@vliz.be>
2021-06-18 10:58:45 +0200

    bump to 0.10.0 and updated dependencies

--- src/das/DAS.java
+++ src/das/DAS.java
@@ -40,7 +40,7 @@ import java.util.concurrent.*;
 
 public class DAS implements DeadThreadListener {
 
-    private static final String version = "0.9.10";
+    private static final String version = "0.10.0";
 
     Path settingsPath = Path.of("settings.xml");
     String workPath=Path.of("").toString();
