284a39ad206b4cb3ed24cd2eb7de5a2bab0458cd
Michiel TJampens <michiel.tjampens@vliz.be>
2021-08-18 13:40:30 +0200

    #8 look back on previous blocks for a math to reuse instead of adding a new one

--- src/util/taskblocks/BlockTree.java
+++ src/util/taskblocks/BlockTree.java
@@ -2,48 +2,77 @@ package util.taskblocks;
 
 import org.tinylog.Logger;
 
+import java.util.Optional;
+
 public class BlockTree {
-    TaskBlock root;
-    TaskBlock last;
+    AbstractBlock root;
+    AbstractBlock last;
     boolean valid =true;
 
-    public BlockTree( TaskBlock origin ){
+    public BlockTree( AbstractBlock origin ){
         root=origin;
         last=origin;
         valid=origin.link(null).build();
     }
-    public static BlockTree trunk( TaskBlock origin ){
+    public static BlockTree trunk( AbstractBlock origin ){
         return new BlockTree((origin));
     }
-    public BlockTree branchOut( TaskBlock branch ){
+    public BlockTree branchOut( AbstractBlock branch ){
         if( !valid )
             return this;
 
+        // Check if the same triggerblock already exist on this level, if so use that instead
+        if( branch instanceof TriggerBlock || branch instanceof CheckBlock){
+            if( getTwigMatch( branch ).map(o->{ last=o; return true; }).orElse(false))
+                return this;
+        }
+        // No matching block found, use the created one
         if( branch.link(last).build() ) {
             last = branch;
         }else{
-            Logger.error("Build of block failed");
+            Logger.error("Build of block failed: "+branch);
             valid=false;
         }
         return this;
     }
+    public Optional<AbstractBlock> getTwigMatch(AbstractBlock block ){
+        if( block instanceof TriggerBlock ){
+            return last.next.stream()
+                    .filter( n -> n instanceof TriggerBlock && ((AbstractBlock)n).ori.equalsIgnoreCase(block.ori))
+                    .map( n -> ((AbstractBlock)n))
+                    .findFirst();
+        }else if( block instanceof CheckBlock ) {
+            return last.next.stream()
+                    .filter(n -> n instanceof CheckBlock && ((AbstractBlock) n).ori.equalsIgnoreCase(block.ori))
+                    .map(n -> ((AbstractBlock) n))
+                    .findFirst();
+        }
+        return Optional.empty();
+    }
     public BlockTree addTwig( TaskBlock twig ){
-        if( !valid )
+        if( !valid ) {
+            Logger.error("Twig not added because invalid: "+twig);
             return this;
-
-        if( !twig.link(last).build() ) {
-            Logger.error("Build of block failed");
+        }
+        if( last instanceof CmdBlock && twig instanceof CmdBlock ){
+            CmdBlock a = (CmdBlock) last;
+            ((CmdBlock) twig).getCmds().forEach(a::addCmd);
+        }else if( !twig.link(last).build() ) {
+            Logger.error("Build of block failed: "+twig);
             valid=false;
         }
         return this;
     }
     public BlockTree branchIn(){
         if( last.getParent().isPresent() ){
-            last = last.getParent().get();
+            last = (AbstractBlock)last.getParent().get();
         }else{
             last = root;
         }
         return this;
+    }
+    public MetaBlock getMetaBlock(){
+        return (MetaBlock) root;
     }
     public boolean start(){
         return root.start();
