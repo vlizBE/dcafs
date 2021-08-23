5b5919f415f2ea554dc64a9d581c6b0710470ea5
Michiel TJampens <michiel.tjampens@vliz.be>
2021-08-23 23:47:00 +0200

    fixed, added check incase split is smaller than expected

--- src/util/math/MathUtils.java
+++ src/util/math/MathUtils.java
@@ -912,7 +912,7 @@ public class MathUtils {
 
         int nulls=0;
         for( int a=0;a<=maxIndex;a++){
-            if( NumberUtils.isCreatable(split[a])) {
+            if( a<split.length&&NumberUtils.isCreatable(split[a]) ) {
                 try {
                     bds[a] = NumberUtils.createBigDecimal(split[a]);
                 }catch(NumberFormatException e) {
