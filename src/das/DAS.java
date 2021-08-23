f4eb5fa10aab9fd4d140e3f65a20cfc1be2d8563
Michiel TJampens <michiel.tjampens@vliz.be>
2021-08-23 09:18:15 +0200

    version bump

--- src/das/DAS.java
+++ src/das/DAS.java
@@ -45,7 +45,7 @@ import java.util.stream.Collectors;
 
 public class DAS implements DeadThreadListener {
 
-    private static final String version = "0.10.10";
+    private static final String version = "0.10.11";
 
     private Path settingsPath = Path.of("settings.xml");
     private String workPath=Path.of("").toString();
