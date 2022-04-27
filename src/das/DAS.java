c8c0aa97fbe4592e88557ca3297ed03a281ad525
Michiel TJampens <60646590+michieltjampens@users.noreply.github.com>
2022-04-27 15:31:14 +0200

    corrected version nr

--- src/das/DAS.java
+++ src/das/DAS.java
@@ -48,7 +48,7 @@ import java.util.stream.Collectors;
 
 public class DAS implements DeadThreadListener {
 
-    private static final String version = "1.0.0";
+    private static final String version = "0.11.10";
 
     private Path settingsPath = Path.of("settings.xml");
     private String workPath=Path.of("").toString();
