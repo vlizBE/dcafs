125c416a8755189589e4f366bca8a4a188cdfe6f
Michiel TJampens <60646590+michieltjampens@users.noreply.github.com>
2022-09-20 18:57:02 +0200

    improved

--- src/util/tools/FileMonitor.java
+++ src/util/tools/FileMonitor.java
@@ -1,9 +1,12 @@
 package util.tools;
 
+import das.Commandable;
+import io.Writable;
 import org.tinylog.Logger;
 import util.xml.XMLtools;
 import worker.Datagram;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.*;
@@ -12,9 +15,9 @@ import java.util.Arrays;
 import java.util.concurrent.*;
 import java.util.function.Function;
 
-import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+import static java.nio.file.StandardWatchEventKinds.*;
 
-public class FileMonitor {
+public class FileMonitor implements Commandable {
 
     WatchService service;
     ScheduledExecutorService watcher = Executors.newSingleThreadScheduledExecutor();
@@ -40,24 +43,28 @@ public class FileMonitor {
             return false;
         /* Check for simple file modification alerts */
         for( var file : XMLtools.getChildElements(ele[0],"file")){
+            var id = XMLtools.getStringAttribute(file,"id","fm"+files.size());
             var p = XMLtools.getPathAttribute(file,"path",root);
             if( p.isEmpty())
                 continue;
             String val = XMLtools.getChildValueByTag(file,"onmodify","");
-            if( val.isBlank())
-                continue;
-            var mon = new ReactionInfo(p.get(), val);
+            boolean read = XMLtools.getBooleanAttribute(file,"read",true);
+
+            var mon = new ReactionInfo(id,p.get(), val);
+            if( read )
+                mon.enableReading();
+
             files.add( mon );
             watcher.submit(() -> watch(mon));
         }
         return true;
     }
-    public void addSimpleWatch( Path p, Function<String,Integer> act){
+    public void addSimpleWatch( String id, Path p, Function<String,Integer> act){
         if(service == null) {
             try {
                 service = FileSystems.getDefault().newWatchService();
                 Files.createFile(p);
-                var mon = new ReactionInfo(p, act, false);
+                var mon = new ReactionInfo(id,p, act, false);
                 files.add( mon );
                 watcher.submit(() -> watch(mon));
             } catch (IOException e) {
@@ -79,36 +86,27 @@ public class FileMonitor {
             var wk = mon.register(service);
             mon.updatePosition();
             service.take();
-            for (var ev : wk.pollEvents()) {
-                String mod = ev.context().toString();
+            for (var event : wk.pollEvents()) {
+                WatchEvent.Kind<?> kind = event.kind();
+                WatchEvent<Path> ev = (WatchEvent<Path>) event;
 
-                if (mod.equals(mon.getFilename())) {
+                String fileName = ev.context().toString();
+
+                if( mon.isFolder()){
+                    if( kind == ENTRY_CREATE ){
+                        Logger.info("File created: "+fileName);
+                        var lines = new ArrayList<String>();
+                        FileTools.readTxtFile(lines,mon.file.resolve(fileName));
+                        lines.forEach(line->mon.sendLine(line));
+                    }
+                }else if( kind == ENTRY_MODIFY && fileName.equals(mon.getFilename()) ){
                     if( mon.hasCmd() ) {
                         dQueue.add(Datagram.system(mon.getCmd()));
                         Logger.info("Executing '"+mon.getCmd()+"' because modified "+mon.getFilename());
                     }
-                    if( mon.handlesReads() ) {
-                        byte[] array = new byte[BUFFER_SIZE];
-                        int read = 0;
-                        try (FileInputStream stream = new FileInputStream(mon.file.toFile())) {
-                            stream.getChannel().position(mon.position);
-                            read = stream.read(array, 0, BUFFER_SIZE);
-                        } catch (IOException r) {
-                           Logger.error(r);
-                        }
-                        if(read > 0) {
-                            String rec = new String(Arrays.copyOfRange(array,0,read));
-                            wk.reset();
-                            wk.cancel();
-                            blank=mon.handleRead(rec);
-                            break;
-                        }else{
-                            mon.handleIssue();
-                        }
-                    }else{
-                        blank=mon.handleNoRead();
-                    }
+                    blank = read(wk,mon,"");
                 }
+
             }
             // reset the key
             wk.reset();
@@ -117,28 +115,85 @@ public class FileMonitor {
         }
         watcher.schedule(()->watch(mon),Math.max(blank,1), TimeUnit.SECONDS);
     }
+    private int read( WatchKey wk, ReactionInfo mon, String sub )  {
+        int blank=0;
+        if( mon.handlesReads() ) {
+            byte[] array = new byte[BUFFER_SIZE];
+            int read = 0;
+            Path toRead = mon.file;
+            if( !sub.isBlank())
+                toRead=toRead.resolve(sub);
+            try (FileInputStream stream = new FileInputStream(toRead.toFile())) {
+                stream.getChannel().position(mon.position);
+                read = stream.read(array, 0, BUFFER_SIZE);
+            } catch (IOException r) {
+                Logger.error(r);
+            }
+            if(read > 0) {
+                String rec = new String(Arrays.copyOfRange(array,0,read));
+                wk.reset();
+                wk.cancel();
+                blank=mon.handleRead(rec);
+            }else{
+                mon.handleIssue();
+            }
+        }else{
+            blank=mon.handleNoRead();
+        }
+        return blank;
+    }
+
+    @Override
+    public String replyToCommand(String[] request, Writable wr, boolean html) {
+        if( request[0].equalsIgnoreCase("fm")){
+            var list = files.stream().filter( fm -> fm.id.equalsIgnoreCase(request[1])).toList();
+            if( list.isEmpty())
+                return "No such filemonitor";
+            list.forEach( fm -> fm.addTarget(wr));
+            return "Target added";
+        }else{
+
+        }
+        return null;
+    }
+
+    @Override
+    public boolean removeWritable(Writable wr) {
+        files.forEach( fm -> fm.targets.remove(wr));
+        return true;
+    }
 
     /**
      * Simple class to contain all required info on what to do after an event
      */
     private static class ReactionInfo {
+        private String id="";
         private final Path file;
         private Function<String, Integer> action;
         private String cmd="";
         private long position=-1;
         private boolean issue=false;
+        private ArrayList<Writable> targets=new ArrayList<>();
 
-        public ReactionInfo(Path file, Function<String, Integer> action, boolean alteration) throws IOException {
+        public ReactionInfo(String id,Path file, Function<String, Integer> action, boolean alteration) throws IOException {
+            this.id=id;
             this.file=file;
             this.action=action;
             if( alteration )
                 position = Files.size(file);
         }
-        public ReactionInfo(Path file, String cmd) {
+        public ReactionInfo(String id, Path file, String cmd) {
+            this.id=id;
             this.file=file;
             this.cmd=cmd;
 
             Logger.info("Monitoring '"+file+"', on modify: "+cmd);
+        }
+        public void enableReading(){
+            position=0;
+        }
+        public void addTarget(Writable wr){
+            targets.add(wr);
         }
         public boolean updatePosition() throws IOException{
             if( position==-1)
@@ -148,7 +203,13 @@ public class FileMonitor {
             return old!=position;
         }
         public WatchKey register( WatchService ws ) throws IOException {
-            return file.getParent().register(ws, ENTRY_MODIFY);
+            if( isFolder()){
+                return file.register(ws, ENTRY_CREATE);
+            }
+            return file.getParent().register(ws, ENTRY_CREATE);
+        }
+        public boolean isFolder(){
+            return Files.isDirectory(file);
         }
         public boolean handlesReads(){
             return position !=-1;
@@ -156,7 +217,14 @@ public class FileMonitor {
         public int handleRead(String rec){
             position += rec.length();
             issue=false;
+            targets.forEach(wr->wr.writeLine(rec));
             return action.apply(rec);
+        }
+        public boolean sendLine(String line){
+            if( targets.isEmpty())
+                return false;
+            targets.forEach(wr->wr.writeLine(line));
+            return true;
         }
         public int handleNoRead(){
             return action.apply("");
