4083d25be95d260bc87330008dd754bb37e64697
Michiel TJampens <60646590+michieltjampens@users.noreply.github.com>
2023-05-04 15:51:48 +0200

    version bump

--- src/das/DAS.java
+++ src/das/DAS.java
@@ -42,7 +42,7 @@ import java.util.concurrent.*;
 
 public class DAS implements Commandable{
 
-    private static final String version = "1.2.3";
+    private static final String version = "1.2.4";
 
     private final Path settingsPath;
     private String workPath;
